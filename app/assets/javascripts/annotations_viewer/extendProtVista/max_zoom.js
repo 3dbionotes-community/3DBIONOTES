ee06a029f14d823c2ba724e136d651dfb8dca94f
Joan Segura <jsegura@cnb.csic.es>
2017-05-30 11:32:20 +0200

    remove extendProtVista && myProtVista from 3DBIONOTES git

--- app/assets/javascripts/annotations_viewer/extendProtVista/max_zoom.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var max_zoom = function (fv){
-	fv.maxZoomSize = 59;
-};
-
-module.exports = max_zoom;
-
--- app/assets/javascripts/annotations_viewer/extendProtVista/rename_structural_features.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var rename_structural_features = function(d){
-	if( "structural" in d){
-		d['structural']['label'] = "UniProt Secondary Structure Data";
-	}
-};
-
-module.exports = rename_structural_features;
