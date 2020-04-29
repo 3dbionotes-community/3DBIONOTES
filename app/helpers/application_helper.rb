c9bd2f40f2b64f53ff0d1aeb7230d773504570c4
Arnau Sanchez <pyarnau@gmail.com>
2020-04-29 13:58:13 +0200

    [webserver] Add static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,20 @@ module ApplicationHelper
       "#{base_title} - #{@title}"
     end
   end
+
+  def menu_item(text, url: nil, action: nil)
+    link_url = if url
+      url
+    elsif action
+      {controller: params[:controller], action: action}
+    else
+      raise "[menu_item] Needs arguments url or action"
+    end
+
+    is_active = params[:action] === action
+
+    content_tag(:li, class: "nav-item", role: "presentation") do
+      link_to(text, link_url, class: ["nav-link", *(is_active ? ["active"]: [])])
+    end
+  end
 end
