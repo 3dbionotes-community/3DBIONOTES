6d08bfd3bcd19cd64990d4c98fb593c107c2bc11
Arnau Sanchez <pyarnau@gmail.com>
2020-05-04 09:36:51 +0200

    Update menu (highlight cv19 section + target on about us)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,13 +8,13 @@ module ApplicationHelper
     end
   end
 
-  def menu_item(text, path)
+  def menu_item(text, path, css_class: nil, target: nil)
     link_url = path.is_a?(String) ? path: params.merge(path)
     is_active = path.is_a?(Hash) ? params[:action] === path[:action] : false
 
-    content_tag(:li, class: "nav-item", role: "presentation") do
-      css_classes = ["nav-link", *(is_active ? ["active"] : [])]
-      link_to(text, link_url, class: css_classes)
+    content_tag(:li, class: ["nav-item", css_class].compact, role: "presentation") do
+      css_classes = ["nav-link", is_active ? ["active"] : nil].compact
+      link_to(text, link_url, class: css_classes, target: target)
     end
   end
 end
