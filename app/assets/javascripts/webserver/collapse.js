350887c1662b7dc60c0ae88c3353ce2e6c3324c8
Arnau Sanchez <pyarnau@gmail.com>
2020-05-04 11:45:35 +0200

    Use click event for collapsable toggle

--- app/assets/javascripts/webserver/collapse.js
+++ app/assets/javascripts/webserver/collapse.js
@@ -1,8 +1,7 @@
 $(() => {
-    $(".collapsable-body").on("show.bs.collapse hide.bs.collapse", (ev) => {
+    $(".collapseToggle").on("click", (ev) => {
         const el = $(ev.currentTarget);
-        $(`[aria-controls=${el.attr("id")}]`)
-            .find("i")
-            .toggleClass("fa-chevron-up fa-chevron-down");
+        console.log(el.attr("id"));
+        el.find("i").toggleClass("fa-chevron-up fa-chevron-down");
     });
 });
