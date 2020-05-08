b86c505aca3158b80d894744f7083da57bd18719
Arnau Sanchez <pyarnau@gmail.com>
2020-05-08 12:09:55 +0200

    [covid19] Render _info partial to helper

--- app/helpers/covid19_helper.rb
+++ app/helpers/covid19_helper.rb
@@ -10,4 +10,18 @@ module Covid19Helper
   def section_popup_title(protein, section)
     [protein[:name], section[:parent], section[:name]].compact.join(" > ")
   end
+
+  def item_info(item)
+    content_tag(:span) do
+      item[:links].map do |link|
+        content_tag(:p) do
+          link_to link[:name], link[:url], {class: "card-link"}
+        end
+      end.join + (
+        item[:related].present? ?
+          content_tag(:p, "Related: #{item[:related].join(', ')}") : ""
+        ) +
+        link_to("External link", item[:external_link])
+    end
+  end
 end
