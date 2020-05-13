10bd31559318a3297c08a6551bf84e574e47eaf2
Arnau Sanchez <pyarnau@gmail.com>
2020-05-13 11:36:08 +0200

    [covid19] Update JSON file, model and views

--- app/helpers/covid19_helper.rb
+++ app/helpers/covid19_helper.rb
@@ -13,15 +13,23 @@ module Covid19Helper
 
   def item_info(item)
     content_tag(:span) do
-      item[:links].map do |link|
-        content_tag(:p) do
-          link_to link[:name], link[:url], {class: "card-link"}
-        end
-      end.join + (
+      safe_join([
+        (content_tag(:p, item[:description]) if item[:description]),
+
+        item[:links].map do |link|
+          content_tag(:p) do
+            safe_join([
+              link[:title] + ":",
+              link_to(link[:name], link[:query_url]),
+              link[:external_url] ? link_to("[External]", link[:external_url]) : "",
+            ], " ")
+          end
+        end.join,
+
         item[:related].present? ?
-          content_tag(:p, "Related: #{item[:related].join(', ')}") : ""
-        ) +
-        link_to("External link", item[:external_link])
+          content_tag(:p, "Related: #{item[:related].join(', ')}") : "",
+        (item[:external_url] ? link_to("External link", item[:external_url]) : "")
+      ])
     end
   end
 end
--- app/models/covid19.rb
+++ app/models/covid19.rb
@@ -34,29 +34,39 @@ class Covid19
 
   private
 
-  def self.get_links(name, style, hash, keys)
-    entry = hash.dig(*keys)
+  def self.get_pdb_redo_links(title, style, hash, keys)
+    entries0 = hash && keys ? (hash.dig(*keys) || []) : []
+    entries = entries0.is_a?(Hash) ? [entries0] : entries0
 
-    urls = if entry.is_a?(Array)
-      entry
-    elsif entry.is_a?(Hash) && entry["links"].is_a?(Hash) && (ext_link = entry.dig("links", "ext_link"))
-      [ext_link]
-    else
-      []
+    with_indexes(entries, title).map do |entry, title_indexed|
+      name, = entry.values_at("name")
+      {
+        title: title_indexed,
+        name: name,
+        style: style,
+        query_url: "/?queryId=PDB-REDO-#{name}&viewer_type=ngl&button=#query",
+        external_url: "https://pdb-redo.eu/db/#{name}",
+      }
     end
-
-    urls.map { |url| {name: name, style: style, url: url} }
   end
 
-  def self.pdb_base(pdb_key)
-    {
-      name: pdb_key,
-      url: "/?queryId=#{pdb_key}&viewer_type=ngl&button=#query",
-      image_url: "https://www.ebi.ac.uk/pdbe/static/entry/#{pdb_key}_deposited_chain_front_image-200x200.png",
-      external_link: "https://www.ebi.ac.uk/pdbe/entry/pdb/#{pdb_key}",
-      related: [],
-      links: [],
-    }
+  def self.with_indexes(entries, title)
+    append_index = entries.size > 1
+    entries.map.with_index(1) { |entry, idx| [entry, append_index ? "#{title} (#{idx})" : title] }
+  end
+
+  def self.get_isolde_links(title, style, hash, keys)
+    entries = hash && keys ? (hash.dig(*keys) || []) : []
+
+    with_indexes(entries, title).map do |entry, title_indexed|
+      entry_name, uuid = entry.values_at("name", "uuid")
+      {
+        title: title_indexed,
+        name: entry_name,
+        style: style,
+        query_url: "/?queryId=ISOLDE-#{title}&uuid=#{uuid}&viewer_type=ngl&button=#query",
+      }
+    end
   end
 
   def self.get_related_keys(data)
@@ -70,16 +80,23 @@ class Covid19
     end
   end
 
+  def self.to_hash(entries)
+    entries.is_a?(Hash) ? entries : entries.map { |key| [key, {}] }.to_h.sort
+  end
+
   def self.parse_pdb(protein, keys)
     entries = protein.dig(*keys) || []
-    entries.sort.map do |pdb_key, pdb_hash|
-      return if pdb_hash.blank?
+    to_hash(entries).map do |pdb_key, pdb_hash|
       {
-        **pdb_base(pdb_key),
+        name: pdb_key,
+        description: pdb_hash["description"],
+        query_url: "/?queryId=#{pdb_key}&viewer_type=ngl&button=#query",
+        image_url: "https://www.ebi.ac.uk/pdbe/static/entry/#{pdb_key}_deposited_chain_front_image-200x200.png",
+        external_url: "https://www.ebi.ac.uk/pdbe/entry/pdb/#{pdb_key}",
         related: get_related_keys(pdb_hash["emdbs"]),
         links: [
-          *get_links("PDB-Redo", :turq, pdb_hash, ["validation", "pdb-redo"]),
-          *get_links("Isolde", :cyan, pdb_hash, ["rebuilt-isolde"]),
+          *get_pdb_redo_links("PDB-Redo", :turq, pdb_hash, ["validation", "pdb-redo"]),
+          *get_isolde_links("Isolde", :cyan, pdb_hash, ["validation", "isolde"]),
         ],
       }
     end.compact
@@ -87,32 +104,42 @@ class Covid19
 
   def self.parse_emdb(protein, keys)
     entries = protein.dig(*keys) || []
-    entries.sort.map do |emdb_key, emdb_value|
+    to_hash(entries).map do |emdb_key, emdb_hash|
       code = emdb_key.split("-")[1]
       {
         name: emdb_key,
-        url: "/?queryId=#{emdb_key}&viewer_type=ngl&button=#query",
+        description: emdb_hash["description"],
+        query_url: "/?queryId=#{emdb_key}&viewer_type=ngl&button=#query",
         image_url: "https://www.ebi.ac.uk/pdbe/static/entry/#{emdb_key}/400_#{code}.gif",
-        related: get_related_keys(emdb_value["pdbs"]),
-        external_link: "https://www.ebi.ac.uk/pdbe/entry/emdb/#{emdb_key}",
+        related: get_related_keys(emdb_hash["pdbs"]),
+        external_url: "https://www.ebi.ac.uk/pdbe/entry/emdb/#{emdb_key}",
         links: [],
       }
     end
   end
 
+  def self.parse_swiss_model(protein, keys)
+    entries = protein.dig(*keys) || []
+    entries.map do |entry|
+      project, model, description = entry.values_at("project", "model", "description")
+      {
+        name: "#{project}-#{model}",
+        description: description,
+        query_url: "/?queryId=SWISS-MODEL-#{project}-#{model}&viewer_type=ngl&button=#query”",
+        image_url: "https://swissmodel.expasy.org/interactive/#{project}/models/#{model}.png",
+        links: [],
+      }
+    end
+  end
+
+  def self.parse_db(protein, keys)
+    parse_pdb(protein, [*keys, "PDB"]) + parse_emdb(protein, [*keys, "EMDB"])
+  end
+
   def self.load_data
     json_path = File.join(__dir__, "../data/cv-data.json")
     data = JSON.parse(open(json_path).read)
-
-    proteins_raw = data.select { |key, value| value.has_key?("PDB") }
-    polyproteins = data.select { |key, value| value.has_key?("proteins") }
-
-    polyproteins_by_protein = polyproteins
-      .flat_map { |name, polyprotein| polyprotein["proteins"].map { |protein| [protein, name] } }
-      .group_by(&:first)
-      .transform_values { |xs| xs.map(&:second) }
-
-    get_proteins_data(proteins_raw, polyproteins_by_protein)
+    get_proteins_data(data["SARS-CoV-2 Proteome"])
   end
 
   def self.card(name, items)
@@ -122,11 +149,6 @@ class Covid19
   def self.card_wrapper(name, subsections)
     subsections2 = subsections.compact.map { |subsection| subsection.merge(parent: name) }
     subsections2.size > 0 ? {name: name, items: [], subsections: subsections2} : nil
-  end
-
-  def self.get_related_items(protein)
-    other_related = protein.dig("Related", "OtherRelated") || []
-    other_related.uniq.map { |pdb_key| pdb_base(pdb_key) }
   end
 
   def self.get_relations(proteins)
@@ -143,22 +165,27 @@ class Covid19
     relations_base.group_by { |k, v| k }.transform_values { |vs| vs.map(&:second).uniq }
   end
 
-  def self.get_proteins_data(proteins_raw, polyproteins_by_protein)
+  def self.get_proteins_data(proteins_raw)
     proteins = proteins_raw.map do |name, protein|
       {
         name: name,
         names: protein["names"],
-        polyproteins: polyproteins_by_protein[name],
+        description: protein["description"],
+        polyproteins: protein["uniprotAccession"],
         sections: [
           card("PDB", parse_pdb(protein, ["PDB"])),
           card("EMDB", parse_emdb(protein, ["EMDB"])),
           card_wrapper("Interactions", [
-            card("PPComplex", parse_pdb(protein, ["Interactions", "PPComplex"])),
+            card("P-P Interactions", parse_db(protein, ["Interactions", "P-P-Interactions"])),
             card("Ligands", parse_pdb(protein, ["Interactions", "Ligands"])),
           ]),
           card_wrapper("Related", [
-            card("SARS-CoV", parse_pdb(protein, ["Related", "SARS-CoV"])),
-            card("Other", get_related_items(protein)),
+            card("SARS-CoV", parse_db(protein, ["Related", "SARS-CoV"])),
+            card("Other", parse_db(protein, ["OtherRelated"]))
+          ]),
+          card_wrapper("Computational Models", [
+            card("Swiss Model", parse_swiss_model(protein, ["CompModels", "swiss-model"])),
+            # TODO: AlphaFold
           ]),
         ].compact,
       }
