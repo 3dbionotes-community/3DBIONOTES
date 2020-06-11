1d08fc4acfd4edfecdb99dfae49c966f179dc656
Adrian Quintana <adrianquintana@gmail.com>
2020-06-11 13:47:11 +0100

    clean up and cosmetics

--- app/assets/javascripts/webserver/popover.js
+++ app/assets/javascripts/webserver/popover.js
@@ -8,31 +8,31 @@ $(() => {
         });
     });
 
-    $('[data-toggle="popover"]').on('show.bs.popover', function (event) {
-        if (event.currentTarget.dataset.api){
+    $('[data-toggle="popover"]').on('shown.bs.popover', function (event) {
+        const el = $(event.currentTarget).find('.no-description');
+        if (el.length > 0 && event.currentTarget.dataset && event.currentTarget.dataset.api){
             $.ajax({
                 url: event.currentTarget.dataset.api,
-                success:function(response){
+                success: function(response) {
                     const keys = Object.keys(response)
-                    if (keys.length == 1) {
+                    if (keys.length === 1) {
                         const type = event.currentTarget.dataset.type;
                         const data = response[keys[0]][0];
-                        let descriptionText = '';
-                        if (type == 'pdb'){
+                        let descriptionText;
+                        if (type === 'pdb') {
                             descriptionText = data.title + '<br/><b>Authors: </b> ' + data.entry_authors.join(', ') + '<br/><b>Released: </b>' + data.release_date
                         }
-                        else{
+                        else if (type === 'emdb'){
                             descriptionText = data.deposition.title + '<br/><b>Authors: </b> ' + data.deposition.authors + '<br/><b>Released: </b>' + data.deposition.map_release_date
                         }
-                        const el = $(event.currentTarget).find('.no-description');
-                        if (el.length > 0){
-                            el.html(descriptionText)
+                        else{
+                            descriptionText = '';
                         }
+                        
+                        el.html(descriptionText)
                     }
-
                 },
-                error: function(response){
-                    const el = $(event.currentTarget).find('.no-description');
+                error: function() {
                     el.html('')
                 }
               });
