4bc9f40b4499b7d5091fa02973f9874cc8cf127a
Arnau Sanchez <pyarnau@gmail.com>
2021-01-08 13:54:37 +0100

    Implement em-validation track

---
+++ app/assets/javascripts/3dbio_viewer/src/domain/entities/Shape.ts
@@ -0,0 +1,13 @@
+export type Shape =
+    | "rectangle"
+    | "bridge"
+    | "diamond"
+    | "chevron"
+    | "catFace"
+    | "triangle"
+    | "wave"
+    | "hexagon"
+    | "pentagon"
+    | "circle"
+    | "arrow"
+    | "doubleBar";
--- app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/provista.types.ts
+++ app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/provista.types.ts
@@ -1,7 +1,8 @@
 import { Color } from "../../../domain/entities/Color";
-import { Pdb } from "../../../domain/entities/Pdb";
+import { Shape } from "../../../domain/entities/Shape";
+import { Variants } from "../../../domain/entities/Variant";
 
-export interface PdbProtvistaData extends Pdb {
+export interface PdbView {
     displayNavigation: boolean;
     displaySequence: boolean;
     displayConservation: boolean;
@@ -11,4 +12,32 @@ export interface PdbProtvistaData extends Pdb {
         alignment: "left" | "right" | "center";
         data: Record<string, Array<{ color: Color[]; text: string }>>;
     };
+    sequence: string;
+    length: number;
+    // https://github.com/ebi-webcomponents/nightingale/tree/master/packages/protvista-track#data-array
+    tracks: TrackView[];
+    variants?: Variants;
+    sequenceConservation?: unknown;
+}
+
+export interface TrackView {
+    label: string;
+    labelType: "text" | "html";
+    overlapping?: boolean;
+    data: Array<{
+        accession: string;
+        type: string; // Displayed in tooltip title
+        label: string; // Supports: text and html.
+        labelTooltip: string; // Label tooltip content. Support text and HTML mark-up
+        overlapping?: boolean;
+        shape: Shape;
+        locations: Array<{ fragments: FragmentView[] }>;
+    }>;
+}
+
+export interface FragmentView {
+    start: number;
+    end: number;
+    color: Color;
+    tooltipContent: string;
 }
