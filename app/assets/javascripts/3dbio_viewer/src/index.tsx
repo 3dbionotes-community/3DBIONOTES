11e9a607f5c98e9db5512e02fe0d5a96ad392e67
Arnau Sanchez <pyarnau@gmail.com>
2021-02-03 12:26:57 +0100

    Disable React strict mode

--- app/assets/javascripts/3dbio_viewer/src/index.tsx
+++ app/assets/javascripts/3dbio_viewer/src/index.tsx
@@ -4,11 +4,6 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./webapp/pages/app/App";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 reportWebVitals();
