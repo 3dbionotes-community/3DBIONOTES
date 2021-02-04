69c52d0a16171f7aacdcbbb9bb26c6ae4dc564d5
Arnau Sanchez <pyarnau@gmail.com>
2021-02-04 13:19:04 +0100

    Add Profiles button component

--- app/assets/javascripts/3dbio_viewer/src/webapp/pages/viewers/Viewers.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/pages/viewers/Viewers.tsx
@@ -4,14 +4,24 @@ import { Protvista } from "../../components/protvista/Protvista";
 import i18n from "../../utils/i18n";
 import styles from "../../components/protvista/Protvista.module.css";
 import { JumpToButton, JumpToButtonProps } from "../../components/protvista/JumpToButton";
+import { ProfilesButton, ProfilesButtonProps } from "../../components/protvista/ProfilesButton";
 
 interface ViewersProps {}
 
 export const Viewers: React.FC<ViewersProps> = () => {
-    const items: JumpToButtonProps["items"] = [
+    const blocksItems: JumpToButtonProps["items"] = [
         { text: i18n.t("Structural information"), id: "structural-info" },
         { text: i18n.t("Map validation"), id: "map-validation" },
         { text: i18n.t("PPI viewer"), id: "ppi" },
+    ];
+
+    const profileItems: ProfilesButtonProps["items"] = [
+        { text: i18n.t("General"), id: "general" },
+        { text: i18n.t("Structural"), id: "structural" },
+        { text: i18n.t("Validation (em-computational models)"), id: "validation" },
+        { text: i18n.t("Drug Design - Experimental"), id: "drug-design" },
+        { text: i18n.t("Omics"), id: "omics" },
+        { text: i18n.t("Biomedicine"), id: "biomedicine" },
     ];
 
     return (
@@ -19,8 +29,8 @@ export const Viewers: React.FC<ViewersProps> = () => {
             <div className={styles.section}>
                 <div className={styles.actions}>
                     <button>{i18n.t("Tools")}</button>
-                    <button>{i18n.t("Profiles")}</button>
-                    <JumpToButton items={items} />
+                    <ProfilesButton items={profileItems} />
+                    <JumpToButton items={blocksItems} />
                 </div>
             </div>
 
