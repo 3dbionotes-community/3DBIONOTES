450cb42e4769950fa4b9b5679cd523dc3417dde0
Arnau Sanchez <pyarnau@gmail.com>
2021-02-05 14:35:00 +0100

    Use new Profiles button

--- app/assets/javascripts/3dbio_viewer/src/webapp/components/viewers/Viewers.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/components/viewers/Viewers.tsx
@@ -6,6 +6,7 @@ import i18n from "../../utils/i18n";
 import styles from "./Viewers.module.css";
 import { JumpToButton, JumpToButtonProps } from "../protvista/JumpToButton";
 import { BasicInfoViewer } from "../BasicInfoViewer";
+import { ProfilesButton, ProfilesButtonProps } from "../protvista/ProfilesButton";
 
 interface ViewersProps {}
 
@@ -17,17 +18,26 @@ export const blocks = {
 };
 
 export const Viewers: React.FC<ViewersProps> = () => {
-    const items: JumpToButtonProps["items"] = React.useMemo(() => {
+    const jumpToItems: JumpToButtonProps["items"] = React.useMemo(() => {
         return _.map(blocks, (attrs, id) => ({ id, ...attrs }));
     }, []);
+
+    const profileItems: ProfilesButtonProps["items"] = [
+        { text: i18n.t("General"), id: "general" },
+        { text: i18n.t("Structural"), id: "structural" },
+        { text: i18n.t("Validation (em-computational models)"), id: "validation" },
+        { text: i18n.t("Drug Design - Experimental"), id: "drug-design" },
+        { text: i18n.t("Omics"), id: "omics" },
+        { text: i18n.t("Biomedicine"), id: "biomedicine" },
+    ];
 
     return (
         <React.Fragment>
             <div className={styles.section}>
                 <div className={styles.actions}>
                     <button>{i18n.t("Tools")}</button>
-                    <button>{i18n.t("Profiles")}</button>
-                    <JumpToButton items={items} />
+                    <ProfilesButton items={profileItems} />
+                    <JumpToButton items={jumpToItems} />
                 </div>
             </div>
 
