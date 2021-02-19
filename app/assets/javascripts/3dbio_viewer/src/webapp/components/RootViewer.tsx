bb57f4bad9a614ce40d25d838a85f1c9686edee9
Arnau Sanchez <pyarnau@gmail.com>
2021-02-19 14:36:12 +0100

    Remove ViewerSelector from RootViewer

--- app/assets/javascripts/3dbio_viewer/src/webapp/components/RootViewer.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/components/RootViewer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Viewers } from "./viewers/Viewers";
 import { MolecularStructure } from "./molecular-structure/MolecularStructure";
-import { ViewerSelector } from "./viewer-selector/ViewerSelector";
 import { SelectionState } from "../view-models/SelectionState";
 
 interface RootViewerProps {
@@ -13,8 +12,6 @@ export const RootViewer: React.FC<RootViewerProps> = props => {
 
     return (
         <div id="viewer">
-            <ViewerSelector selection={selection} />
-
             <div id="left">
                 <MolecularStructure selection={selection} />
             </div>
