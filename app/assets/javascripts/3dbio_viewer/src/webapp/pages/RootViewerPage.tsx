27f2a54153af0ca44f4eea8212d09a071ae9cd06
Arnau Sanchez <pyarnau@gmail.com>
2021-02-23 12:56:29 +0100

    Show default PDB on root

--- app/assets/javascripts/3dbio_viewer/src/webapp/pages/RootViewerPage.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/pages/RootViewerPage.tsx
@@ -3,9 +3,9 @@ import { Route } from "react-router-dom";
 import { RootViewer } from "../components/RootViewer";
 import { useViewerSelector } from "../components/viewer-selector/viewer-selector.hooks";
 
-export const RootViewerPage: React.FC<{ selector: string }> = React.memo(props => {
-    const [selection, setSelection] = useViewerSelector(props.selector);
-    
+export const RootViewerPage: React.FC<{ selector?: string }> = React.memo(props => {
+    const [selection, setSelection] = useViewerSelector(props.selector || "6w9c");
+
     return <RootViewer selection={selection} onSelectionChange={setSelection} />;
 });
 
