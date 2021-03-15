53a1fab79c1bfe7e2c167f937048a7dc9c962272
Arnau Sanchez <pyarnau@gmail.com>
2021-03-15 11:45:40 +0100

    Prefix proxy routes

--- app/assets/javascripts/3dbio_viewer/src/setupProxy.js
+++ app/assets/javascripts/3dbio_viewer/src/setupProxy.js
@@ -3,20 +3,20 @@ const apicache = require("apicache");
 
 module.exports = function (app) {
     proxyRoutes(app, {
-        prefix: "3dbionotes",
+        prefix: "/3dbionotes",
         target: "https://3dbionotes.cnb.csic.es",
     });
 
     proxyRoutes(app, {
-        prefix: "ebi",
+        prefix: "/ebi",
         target: "https://www.ebi.ac.uk",
     });
 };
 
 function proxyRoutes(app, options) {
     const { prefix, target } = options;
-    const routes = ["/" + prefix];
-    const path = `^/${prefix}/`;
+    const routes = [prefix];
+    const path = `^${prefix}/`;
 
     const proxyOptions = {
         target,
