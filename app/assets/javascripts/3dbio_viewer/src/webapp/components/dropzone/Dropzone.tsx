3daba110cb53fc3df92c349903ed3d69e01a3176
Arnau Sanchez <pyarnau@gmail.com>
2021-04-16 10:24:54 +0200

    Move auxiliar functions/styles to the bottom of Dropzone component

--- app/assets/javascripts/3dbio_viewer/src/webapp/components/dropzone/Dropzone.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/components/dropzone/Dropzone.tsx
@@ -3,47 +3,9 @@ import { DropzoneOptions, DropzoneRootProps, useDropzone } from "react-dropzone"
 import styled from "styled-components";
 import i18n from "../../utils/i18n";
 
-const getColor = (props: DropzoneRootProps) => {
-    if (props.isDragAccept) {
-        return "#00e676";
-    }
-    if (props.isDragReject) {
-        return "#ff1744";
-    }
-    if (props.isDragActive) {
-        return "#2196f3";
-    }
-    return "#eeeeee";
-};
-
-const Shade = styled.div<DropzoneRootProps>`
-    display: flex;
-    place-content: center;
-    text-shadow: 1px 1px 10px black;
-    padding: 60px;
-    align-items: center;
-    border-width: 2px;
-    border-radius: 2px;
-    border-color: ${props => getColor(props)};
-    border-style: dashed;
-    background-color: #fafafa;
-    outline: none;
-    transition: border 0.24s ease-in-out;
-    height: 10px;
-    background-color: rgba(10, 10, 10, 0.5);
-    cursor: pointer;
-`;
-
-const Text = styled.p`
-    text-align: center;
-    font-size: 18px;
-    color: white;
-`;
-
 export interface DropzoneRef {
     openDialog: () => void;
     files: File[];
-    //setErrorMessage: () => void;
 }
 
 export const Dropzone = React.forwardRef(
@@ -82,3 +44,40 @@ export const Dropzone = React.forwardRef(
 export function getFile(fileRef: React.RefObject<DropzoneRef>): File | undefined {
     return fileRef.current?.files[0];
 }
+
+const getColor = (props: DropzoneRootProps) => {
+    if (props.isDragAccept) {
+        return "#00e676";
+    }
+    if (props.isDragReject) {
+        return "#ff1744";
+    }
+    if (props.isDragActive) {
+        return "#2196f3";
+    }
+    return "#eeeeee";
+};
+
+const Shade = styled.div<DropzoneRootProps>`
+    display: flex;
+    place-content: center;
+    text-shadow: 1px 1px 10px black;
+    padding: 60px;
+    align-items: center;
+    border-width: 2px;
+    border-radius: 2px;
+    border-color: ${props => getColor(props)};
+    border-style: dashed;
+    background-color: #fafafa;
+    outline: none;
+    transition: border 0.24s ease-in-out;
+    height: 10px;
+    background-color: rgba(10, 10, 10, 0.5);
+    cursor: pointer;
+`;
+
+const Text = styled.p`
+    text-align: center;
+    font-size: 18px;
+    color: white;
+`;
