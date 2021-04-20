e67be8f2ac237869a4dc1b27c99e365e6b1ded49
Arnau Sanchez <pyarnau@gmail.com>
2021-04-20 12:32:11 +0200

    Type i18n helper

--- app/assets/javascripts/3dbio_viewer/src/webapp/utils/i18n.ts
+++ app/assets/javascripts/3dbio_viewer/src/webapp/utils/i18n.ts
@@ -1,3 +1,9 @@
 import i18n from "d2-ui-components/locales";
 
-export default i18n;
+const i18nWrapper = {
+    t(s: string, namespace?: object): string {
+        return i18n.t(s, namespace);
+    },
+};
+
+export default i18nWrapper;
