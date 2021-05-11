e0d676c3d7b4eadc43b0963d21222e1649af2ca6
Arnau Sanchez <pyarnau@gmail.com>
2021-05-11 10:12:24 +0200

    Assume all uniprot parsing XML as optional attributes

--- app/assets/javascripts/3dbio_viewer/src/data/repositories/protvista/uniprot.ts
+++ app/assets/javascripts/3dbio_viewer/src/data/repositories/protvista/uniprot.ts
@@ -8,15 +8,15 @@ export interface UniprotResponse {
         entry: Array<{
             accession: string[];
             name: string[];
-            protein: Array<{
-                recommendedName: Array<{
-                    fullName: Array<{ _: string }>;
+            protein?: Array<{
+                recommendedName?: Array<{
+                    fullName?: Array<{ _: string }>;
                 }>;
             }>;
-            gene: Array<{
+            gene?: Array<{
                 name: Array<{ $: { type: "primary" | "ORF" }; _: string }>;
             }>;
-            organism: Array<{
+            organism?: Array<{
                 name: Array<{ _: string }>;
             }>;
         }>;
@@ -27,10 +27,10 @@ export function getProtein(proteinId: string, res: UniprotResponse | undefined):
     const entry = res?.uniprot.entry[0];
     if (!entry) return { id: proteinId };
 
-    const name = entry.protein[0]?.recommendedName[0]?.fullName[0]?._;
-    const geneEntries = entry.gene[0]?.name || [];
-    const geneEntry = entry.gene[0]?.name.find(g => g.$.type === "primary") || geneEntries[0];
-    const organismEntries = _.compact(entry.organism[0]?.name.map(x => x._));
+    const name = entry.protein?.[0]?.recommendedName?.[0]?.fullName?.[0]?._;
+    const geneEntries = entry.gene?.[0]?.name || [];
+    const geneEntry = entry.gene?.[0]?.name.find(g => g.$.type === "primary") || geneEntries[0];
+    const organismEntries = _.compact(entry.organism?.[0]?.name.map(x => x._));
 
     const organism = [
         ..._.take(organismEntries, 1),
