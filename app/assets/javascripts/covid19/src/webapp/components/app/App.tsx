80331692da21a1fabeb0c27504c813b51f5002ef
Arnau Sanchez <pyarnau@gmail.com>
2021-06-07 13:50:08 +0200

    Use data/covid19.json in main App component

--- app/assets/javascripts/covid19/src/webapp/components/app/App.tsx
+++ app/assets/javascripts/covid19/src/webapp/components/app/App.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@material-ui/core";
 import React from "react";
+import testCovid19Data from "../../../data/covid19.json";
 
-export const App: React.FC = () => {
+interface Covid19Data {
+    proteins: unknown[];
+}
+
+interface AppProps {
+    data?: Covid19Data;
+}
+
+export const App: React.FC<AppProps> = props => {
+    const data: Covid19Data = props.data || testCovid19Data;
+
     return (
         <div>
-            <div>App component</div>
+            <div>App component: {data.proteins.length} proteins</div>
 
             <Button variant="contained" color="primary">
                 Does nothing
