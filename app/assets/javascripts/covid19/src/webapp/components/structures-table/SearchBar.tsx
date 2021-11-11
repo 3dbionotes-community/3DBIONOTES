c86a98abbcc35109f4bac60c93ad6e87813b90af
Jocelyn Dunkley <jocelyn.dunkley09@gmail.com>
2021-11-11 19:17:38 +0100

    added examples to the search bar

--- app/assets/javascripts/covid19/src/webapp/components/structures-table/SearchBar.tsx
+++ app/assets/javascripts/covid19/src/webapp/components/structures-table/SearchBar.tsx
@@ -2,6 +2,7 @@ import { TextField, TextFieldProps } from "@material-ui/core";
 import React from "react";
 import i18n from "../../../utils/i18n";
 import { styles } from "./Toolbar";
+import { SearchExampleButton } from "./SearchExampleButton";
 
 export interface SearchBarProps {
     value: string;
@@ -19,12 +20,26 @@ export const SearchBar: React.FC<SearchBarProps> = React.memo(props => {
     );
 
     return (
-        <TextField
-            type="search"
-            value={value || ""}
-            style={styles.search}
-            onChange={setValueFromEv}
-            placeholder={i18n.t("Search")}
-        />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <TextField
+                type="search"
+                value={value || ""}
+                style={styles.search}
+                onChange={setValueFromEv}
+                placeholder={i18n.t("Search")}
+            />
+            <div style={searchBarStyle.exampleRow}>
+                <SearchExampleButton setValue={setValue} exampleValue="6YOR" />
+                <SearchExampleButton setValue={setValue} exampleValue="Homo sapiens" />
+                <SearchExampleButton setValue={setValue} exampleValue="SARS-CoV-2" />
+            </div>
+        </div>
     );
 });
+
+const searchBarStyle = {
+    exampleRow: {
+        display: "flex",
+        marginTop: 5,
+    },
+};
