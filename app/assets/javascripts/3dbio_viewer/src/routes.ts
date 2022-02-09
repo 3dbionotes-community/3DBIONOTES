7775e262d995abec686ce402796b619ae5939e8c
Arnau Sanchez <pyarnau@gmail.com>
2022-02-09 14:05:36 +0100

    Use relative path for bionotesDev route on PRO deploy

--- app/assets/javascripts/3dbio_viewer/src/routes.ts
+++ app/assets/javascripts/3dbio_viewer/src/routes.ts
@@ -4,7 +4,7 @@ const isDev = process.env.NODE_ENV === "development";
 
 export const routes = {
     bionotes: isDev ? "/3dbionotes" : "", // Use relative requests on 3dbionotes PRO
-    bionotesDev: isDev ? "/rinchen-dos" : "http://rinchen-dos.cnb.csic.es:8882",
+    bionotesDev: isDev ? "/rinchen-dos" : "",
     ebi: isDev ? "/ebi" : "https://www.ebi.ac.uk",
     uniprot: isDev ? "/uniprot" : "https://www.uniprot.org",
 };
