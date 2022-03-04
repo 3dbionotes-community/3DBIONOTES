12f7e93d9b8736faa86e219e526b3227ab648a36
p3rcypj <pablo@percypj.com>
2022-03-04 14:45:14 +0100

    Add link to PDB/EMDB img and title

--- app/assets/javascripts/covid19/src/webapp/components/structures-table/cells/TitleCell.tsx
+++ app/assets/javascripts/covid19/src/webapp/components/structures-table/cells/TitleCell.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { CellProps } from "../Columns";
 import { BadgeLink } from "../BadgeLink";
 
+export const urlPrefix = "https://3dbionotes.cnb.csic.es/?queryId=";
+
 export const TitleCell: React.FC<CellProps> = React.memo(props => {
     const structure = props.row;
-    const queryLinkToUse = structure.emdb?.queryLink || structure.pdb?.queryLink;
+    const queryLinkToUse =
+        urlPrefix + (structure.emdb?.id.toUpperCase() || structure.pdb?.id.toLowerCase());
 
     return (
         <>
