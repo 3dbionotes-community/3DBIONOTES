e99bdbe1e2ce7ddc0473c88e233a5633dd009edd
p3rcypj <pablo@percypj.com>
2022-03-10 12:10:16 +0100

    Add GA to main app

--- app/assets/javascripts/webserver/webserver_init.js
+++ app/assets/javascripts/webserver/webserver_init.js
@@ -5,11 +5,55 @@ function gtag() {
 }
 gtag("js", new Date());
 
-gtag("config", "UA-222241504-1");
+gtag("config", "UA-93698320-4");
 
 var $j = jQuery.noConflict();
 
+function createFunctionWithTimeout(callback, opt_timeout) {
+    var called = false;
+    function fn() {
+        if (!called) {
+            called = true;
+            callback();
+        }
+    }
+    setTimeout(fn, opt_timeout || 1000);
+    return fn;
+}
+
 $j(document).ready(function () {
+    $j("#btn-contact").bind("click", () => {
+        gtag("event", "contact", {
+            event_category: "home_page",
+            event_label: "Clicked contact",
+        });
+    });
+    $j("#cta-covid").bind("click", () => {
+        gtag("event", "view_page", {
+            event_category: "call_to_action",
+            event_label: "/covid19",
+        });
+        e.preventDefault();
+    });
+    $j("#cta-example").bind("click", () => {
+        gtag("event", "view_item", {
+            event_category: "call_to_action",
+            event_label: $j("#cta-example").data("id"),
+        });
+    });
+
+    const form = document.getElementById("form-search");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        gtag("event", "search", {
+            event_category: "home_page",
+            event_label: $j("#input-search").val(),
+            event_callback: createFunctionWithTimeout(function () {
+                form.submit();
+            }),
+        });
+    });
+
     $j(".submit_form").submit(function (event) {
         if (!$j("#structure_file").val()) {
             alert("No file selected. Please select a coordinates file.");
