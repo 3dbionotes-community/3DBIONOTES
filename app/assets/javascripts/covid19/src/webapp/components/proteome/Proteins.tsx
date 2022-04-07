c9a5f09ae0223b75dca5fd652b03ecde481ef2bf
p3rcypj <pablo@percypj.com>
2022-04-07 14:39:25 +0200

    Add domain ORF1a and ORF1b, synonyms and minor refactors

--- app/assets/javascripts/covid19/src/webapp/components/proteome/Proteins.tsx
+++ app/assets/javascripts/covid19/src/webapp/components/proteome/Proteins.tsx
@@ -23,13 +23,13 @@ const build = (type: "pdb" | "emdb", id: string): DB => ({
 
 const proteins: Protein[] = [
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "ORF1a",
         def:
             "M408.31,151.89l24.58,88.52-.18.05a276.34,276.34,0,0,0-86.93,41.71A278.93,278.93,0,0,0,275.68,355,276.14,276.14,0,0,0,230.4,491.15q-.5,8.22-.49,16.56a279.07,279.07,0,0,0,1.8,31.7A276.12,276.12,0,0,0,259,631.6a277.46,277.46,0,0,0,25.27,41.12.7.7,0,0,0,.13.17L210.05,728a2.09,2.09,0,0,0-.14-.17,372.72,372.72,0,0,1-33.68-54.86,369.2,369.2,0,0,1,62.6-419.16A370.36,370.36,0,0,1,287.2,211c1.7-1.26,3.41-2.5,5.11-3.71a361.87,361.87,0,0,1,55.41-32.89,369.15,369.15,0,0,1,60.41-22.47Z",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP1",
         synonyms: "NSP1, Non-structural protein 1",
         pdb: build("pdb", "7K7P"),
@@ -39,7 +39,7 @@ const proteins: Protein[] = [
             "NSP1: Leader protein of 1a and 1ab polyproteins. NSP1 inhibits host cell translation by interacting with the 40S small ribosomal subunit. Slowing down infected host protein production, NSP1 facilitates efficient viral protein translation and evasion from host immune response, thus helping the virus to proliferate.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP2",
         synonyms: "NSP2, Non-structural protein 2",
         pdb: build("pdb", "7MSW"),
@@ -50,13 +50,13 @@ const proteins: Protein[] = [
             "NSP2: Although its function is still unknown, NSP2 has been involved in several viral processes. Its highly-conserved Zn2+ binding site suggests that nsp2 binds RNA. It also seems to interact with host infected cell endosomes through cytoskeletal elements and with modulators of translation. ",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         def:
             "M283.48,196.66a383.88,383.88,0,0,0-53.85,47l-25.4-24.06a420.87,420.87,0,0,1,58.92-51.42Z",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "Ubl-1",
         synonyms: "Ubl-1, Ubiquitin-like domain 1",
@@ -66,7 +66,7 @@ const proteins: Protein[] = [
         description: "UBL-1: Ubiquitin-like domain 1 of NSP3.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "MacroDomain",
         synonyms: "MacroDomain, Macrodomain I, Mac1",
@@ -77,7 +77,7 @@ const proteins: Protein[] = [
             "MacroDomain I of NSP3 (Mac1): Domain that recognizes and hydrolizes the ADP-ribose label from proteins associated with the host innate immune response.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "SUD-N",
         synonyms: "SUD-N, SARS-unique domain N, Macrodomain II, Mac2",
@@ -88,7 +88,7 @@ const proteins: Protein[] = [
             "SARS-unique domain N of NSP3 (Mac2 ): This domain interacts with the human Paip1 (PABP-interacting protein) and enhances viral protein synthesis.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "SUD-M",
         synonyms: "SUD-M, SARS-unique domain M, Macrodomain III, Mac3",
@@ -99,7 +99,7 @@ const proteins: Protein[] = [
             "SARS-unique domain M of NSP3 (Mac3): Domain that interacts with the human Paip1 (PABP-interacting protein) and enhances viral protein synthesis.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "SUD-C",
         synonyms: "SUD-C, SARS-unique domain C, domain preceding Ubl-2 and PL-Pro, DPUP",
@@ -109,7 +109,7 @@ const proteins: Protein[] = [
         description: "SARS-unique domain C of NSP3 (DPUP).",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "Ubl-2",
         synonyms: "Ubl-2, Ubiquitin-like domain 2",
@@ -117,7 +117,7 @@ const proteins: Protein[] = [
         description: "UBL-2: Ubiquitin-like domain 2 of NSP3.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "PL-Pro",
         synonyms: "NSP3, PL-Pro, Papain-like proteinase, Papain-like protease",
@@ -128,7 +128,7 @@ const proteins: Protein[] = [
             "PL-Pro: Domain Papain-like proteinase of NSP3. PL-Pro processes proteolytically three cleavage sites located at the N-terminus of the polyproteins 1a and 1ab. In addition, PL-Pro possesses a deubiquitinating/deISGylating activity and processes polyubiquitin chains from cellular substrates. It participates together with NSP4 in the assembly of virally-induced cytoplasmic double-membrane vesicles necessary for viral replication. It also antagonizes innate immune induction of type I interferon by blocking phosphorylation, dimerization and subsequent nuclear translocation of host IRF3. It prevents host cell NF-kappa-B signaling as well.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "NAB",
         synonyms: "NAB, Nucleic acid binding domain",
@@ -138,7 +138,7 @@ const proteins: Protein[] = [
         description: "NAB: Nucleic acid binding domain of NSP3.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP3",
         domain: "Y3 domain",
         synonyms: "Y3 domain",
@@ -148,7 +148,7 @@ const proteins: Protein[] = [
         description: "Y3 domain of NSP3.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP4",
         synonyms: "NSP4, Non-structural protein 4",
         pdb: build("pdb", "N/A"),
@@ -158,7 +158,7 @@ const proteins: Protein[] = [
             "NSP4: Protein product involved in the assembly of virally-induced cytoplasmic double-membrane vesicles necessary for viral replication.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP5",
         synonyms:
             "NSP5, Non-structural protein 5, Main Protease, MPro, 3C-like protease, 3CL-Pro, Main proteinase, 3C-like proteinase",
@@ -169,7 +169,7 @@ const proteins: Protein[] = [
             "NSP5: Protease that processes proteolytically the C-terminus of the polyproteins 1a and 1ab at seven and twelve sites, respectively.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP6",
         synonyms: "NSP6, Non-structural protein 6",
         pdb: build("pdb", "N/A"),
@@ -179,7 +179,7 @@ const proteins: Protein[] = [
             "NSP6: Membrane protein that forms a double-membrane vesicle along with NSP3 and NSP4. At late states of viral infection it inhibits host cell autophagy by reducing autophagosome size, thus preventing innate and adaptive host immune responses.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP7",
         synonyms: "NSP7, Non-structural protein 7",
         pdb: build("pdb", "7C2K"),
@@ -190,7 +190,7 @@ const proteins: Protein[] = [
             "NSP7: As cofactor of NSP12, NSP7 contributes to accomodate the template-product RNA, together with NSP8, in the RNA-dependent RNA polymerase (RdRp) catalytic complex. ",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP8",
         synonyms: "NSP8, Non-structural protein 8",
         pdb: build("pdb", "7C2K"),
@@ -201,7 +201,7 @@ const proteins: Protein[] = [
             "NSP8: As cofactor of NSP12, NSP8 contributes to accomodate the template-product RNA, together with NSP7, in the RNA-dependent RNA polymerase (RdRp) catalytic complex. ",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP9",
         synonyms: "NSP9, Non-structural protein 9",
         pdb: build("pdb", "6WXD"),
@@ -211,7 +211,7 @@ const proteins: Protein[] = [
             "NSP9: Able to bind ssRNA, NSP9 forms a covalent RNA-protein intermediate. This RNA is first transferred by the NiRAN domain of NSP12 to NSP9 . Then, the NiRAN NSP12 domain transfers the RNA to GDP to start the building process of the RNA cap structure. Bound to the NSP12 NiRAN domain, NSP9 inhibits the guanylyl tranferase enzymatic activity of NSP12.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP10",
         synonyms: "NSP10, Non-structural protein 10",
         pdb: build("pdb", "6ZPE"),
@@ -221,7 +221,7 @@ const proteins: Protein[] = [
             "NSP10: Stimulating both NSP14 exoribonuclease and NSP16 methyltransferase activities, NSP10 plays an essential role in viral mRNA proofreading and cap methylation.",
     },
     {
-        gen: "orf1a",
+        gen: "ORF1a",
         name: "NSP11",
         synonyms: "NSP11, Non-structural protein 11",
         pdb: build("pdb", "N/A"),
@@ -230,13 +230,13 @@ const proteins: Protein[] = [
         description: "NSP11: Non-structural protein 11. ",
     },
     {
-        gen: "orf1b",
+        gen: "ORF1b",
         name: "ORF1b",
         def:
             "M819.07,706.39c-1.13,1.8-2.3,3.6-3.49,5.38A369.58,369.58,0,0,1,313.86,823c-3.58-2.2-7.12-4.45-10.64-6.78-38.84-25.66-65.47-50.89-93.17-88.29l74.39-55.07a279,279,0,0,0,77.73,71.27,277,277,0,0,0,145.14,41,279.17,279.17,0,0,0,59.74-6.45,275.75,275.75,0,0,0,97.19-42.19,279,279,0,0,0,74.36-75.58q1.33-2,2.63-4Z",
     },
     {
-        gen: "orf1b",
+        gen: "ORF1b",
         name: "NSP12",
         synonyms:
             "NSP12, Non-structural protein 12, RNA-directed RNA polymerase, RNA-dependent RNA polymerase, RdRp",
@@ -248,7 +248,7 @@ const proteins: Protein[] = [
             "NSP12: As main component of the replication-transcription complex (RTC), NSP12 catalyzes the synthesis of complementary RNA (negative) using the positive RNA as template, as well as the synthesis of mRNA starting from the complementary negative RNA as template. With a second enzymatic activity of the NSP12,  guanylyl tranferase from the NiRAN domain, also contributes to the mRNA cap synthesis.",
     },
     {
-        gen: "orf1b",
+        gen: "ORF1b",
         name: "NSP13",
         synonyms: "NSP13, Non-structural protein 13, Helicase",
         pdb: build("pdb", "5ROB"),
@@ -258,7 +258,7 @@ const proteins: Protein[] = [
             "NSP13: Its helicase RNA duplex-unwinding activity contributes to elongate the RNA product. Additionally, its fosfatase activity catalizes the first step of the mRNA cap synthesis.",
     },
     {
-        gen: "orf1b",
+        gen: "ORF1b",
         name: "NSP14",
         synonyms:
             "NSP14, Non-structural protein 14, Proofreading exoribonuclease, 3'-5' exoribonuclease, N7 methyltransferase, ExoN/N7-MTase",
@@ -269,7 +269,7 @@ const proteins: Protein[] = [
             "NSP14: Enzyme with two different catalytic activities: Exoribonuclease activity in its N-terminal end, and N7-methyltransferase activity in its C-terminal end. Its proofreading exoribonuclease lowers viral sensitivity to nucleoside analogs.",
     },
     {
-        gen: "orf1b",
+        gen: "ORF1b",
         name: "NSP15",
         synonyms:
             "NSP15, Non-structural protein 15, Endoribonuclease, Uridylate-specific endoribonuclease, Endornase, NendoU",
@@ -280,7 +280,7 @@ const proteins: Protein[] = [
             "NSP15: As uridylate-specific enzyme, NSP15 hydrolizes both ssRNA and dsRNA, probably to avoid trigger the host inmune response.",
     },
     {
-        gen: "orf1b",
+        gen: "ORF1b",
         name: "NSP16",
         synonyms: "NSP16, Non-structural protein 16, 2'-O-methyltransferase",
         pdb: build("pdb", "7L6T"),
@@ -290,7 +290,7 @@ const proteins: Protein[] = [
             "NSP16: Methyltransferase that catalyzes the last reaction to build the viral mRNA cap, the methylation of the ribose 2'-OH. ",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "Spike",
         synonyms: "Spike, Protein S, Spike glycoprotein, Surface Glycoprotein",
         pdb: build("pdb", "6VSB"),
@@ -301,7 +301,7 @@ const proteins: Protein[] = [
             "Spike: Decorating surface glycoprotein that protrudes from the viral outer membrane. Several conformational changes allow its interaction with the host receptor ACE2 and membrane fusion, thus initiating the infection. ",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "ORF3",
         synonyms: "ORF3, ORF3a, Accessory protein 3a,  Protein U274, Protein X1",
         pdb: build("pdb", "6XDC"),
@@ -312,7 +312,7 @@ const proteins: Protein[] = [
             "ORF3: As a viroporin, ORF3 forms cellular dimeric nonselective Ca2+ permeable cation channels that become active by oligomerizing into tetramers or higher-order oligomers. This channel activity in cells could be relevant to promote viral maturation through inhibition of autophagy and disruption of lysosomes.",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "Envelope Protein",
         synonyms:
             "Protein E, Envelope small membrane protein, Protein E. Envelope membrane protein",
@@ -323,7 +323,7 @@ const proteins: Protein[] = [
             "Protein E: Envelope small membrane protein. As a viroporin, it self-assembles in host membranes forming pentameric protein pores that can affect the integrity of the lipid bilayer or disrupt the membrane potential, thus facilitating the release of viral particles from host cells.",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "Membrane Protein",
         synonyms: "Protein M, Membrane protein",
         pdb: build("pdb", "N/A"),
@@ -333,7 +333,7 @@ const proteins: Protein[] = [
             "Protein M: Membrane protein. Abundant transmembrane component of the viral envelope that functions as a  organizational hub for virion assembly through its binding to the Spike and Nucleocapsid proteins in the outer and inner membrane surface, respectively. ",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "ORF6",
         synonyms: "ORF6, Accessory Protein 6, Accessory Factor 6",
         pdb: build("pdb", "7VPH"),
@@ -343,7 +343,7 @@ const proteins: Protein[] = [
             "ORF6: Although it is ot necessary for viral invasion and replication, ORF6 plays a relevant role in immune evasion. It directly interacts with the host complex Rae1-Nup98 preventing the nuclear export of host mRNA, antagonizing interferon signaling.",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "ORF7a",
         synonyms: "ORF7a Protein, Accessory protein 7a",
         pdb: build("pdb", "7CI3"),
@@ -353,7 +353,7 @@ const proteins: Protein[] = [
             "ORF7a: Although dispensable for virus replication in cell culture, ORF7a acts as a immunomodulator factor that binds to immune cells and triggers inflammatory responses.",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "ORF7b",
         synonyms: "ORF7b, Accessory protein 7b",
         pdb: build("pdb", "N/A"),
@@ -362,7 +362,7 @@ const proteins: Protein[] = [
         description: "ORF7b: Small accessory protein 7b.",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "ORF8",
         synonyms: "ORF8, Non-structural protein 8",
         pdb: build("pdb", "7F5F"),
@@ -372,13 +372,13 @@ const proteins: Protein[] = [
             "ORF8: With a core fold similar to ORF7a, the structure of ORF8 reveals two novel dimer interfaces that allow forming host large-scale assemblies, potentially mediating immune suppression and evasion activities.",
     },
     {
-        gen: "nucleoprotein",
+        gen: "Nucleoprotein",
         name: "Nucleoprotein",
         def:
             "M747.26,226.82l-59.76,70a277.6,277.6,0,0,0-60.75-39.53l39.55-83C697.82,189.32,720.7,204.16,747.26,226.82Z",
     },
     {
-        gen: "nucleoprotein",
+        gen: "Nucleoprotein",
         name: "Nucleoprotein",
         domain: "NTD",
         synonyms:
@@ -388,7 +388,7 @@ const proteins: Protein[] = [
             "Nucleoprotein N-terminal domain: The nucleoprotein (N protein) is involved in viral assembly, replication and host immune response regulation. Its N-terminal domain (N-NTD) is responsible for RNA binding and packaging of the positive strand viral genome RNA into a helical ribonucleocapsid (RNP).",
     },
     {
-        gen: "nucleoprotein",
+        gen: "Nucleoprotein",
         name: "Nucleoprotein",
         domain: "CTD",
         synonyms:
@@ -398,7 +398,7 @@ const proteins: Protein[] = [
             "Nucleoprotein C-terminal domain: The nucleoprotein (N protein) is involved in viral assembly, replication and host immune response regulation. Its C-terminal domain (N-CTD) is responsible for RNA binding and packaging of the positive strand viral genome RNA into a helical ribonucleocapsid (RNP).",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "ORF9b",
         synonyms: "ORF9b, Protein 9b",
         pdb: build("pdb", "7KDT"),
@@ -409,7 +409,7 @@ const proteins: Protein[] = [
             "ORF9b: The gene encoding this small protein overlaps the N gene. ORF9b protein interacts and blocks the substrate binding site of the host Tom70, import receptor of the translocase from the mitochondrial outer membrane (TOM) complex, involved in the activation of the mitochondrial antiviral signaling leading to apoptosis upon viral infection. In addition, ORF9b may modulate interferon and apoptosis signaling avoiding the interaction between Tom70 and Hsp70 and Hsp90 chaperones.",
     },
     {
-        gen: "remaining",
+        gen: "Remaining",
         name: "ORF10",
         synonyms: "ORF10, Hypothetical ORF10 Protein.",
         pdb: build("pdb", "N/A"),
@@ -420,10 +420,10 @@ const proteins: Protein[] = [
 ];
 
 const protWithDetails = proteins.map(prot => ({
-    gen: prot.gen,
     name: prot.name,
     def: prot.def,
     details: {
+        gen: prot.gen,
         synonyms: prot.synonyms,
         domain: prot.domain,
         description: prot.description,
@@ -432,7 +432,7 @@ const protWithDetails = proteins.map(prot => ({
     },
 }));
 
-export const Orf1a = protWithDetails.filter(prot => prot.gen === "orf1a");
-export const Orf1b = protWithDetails.filter(prot => prot.gen === "orf1b");
-export const Nucleoprotein = protWithDetails.filter(prot => prot.gen === "nucleoprotein");
-export const Remaining = protWithDetails.filter(prot => prot.gen === "remaining");
+export const Orf1a = protWithDetails.filter(prot => prot.details.gen === "ORF1a");
+export const Orf1b = protWithDetails.filter(prot => prot.details.gen === "ORF1b");
+export const Nucleoprotein = protWithDetails.filter(prot => prot.details.gen === "Nucleoprotein");
+export const Remaining = protWithDetails.filter(prot => prot.details.gen === "Remaining");
