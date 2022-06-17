d449ec5b90a3aa4b29103c699b4169ea6330a55c
Arnau Sanchez <pyarnau@gmail.com>
2022-06-17 13:32:18 +0200

    Add simple D3 example in ProtvistaPdbValidation

--- app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/ProtvistaPdbValidation.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/ProtvistaPdbValidation.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import * as d3Module from "d3";
 import { ProtvistaPdb, ProtvistaPdbProps } from "./ProtvistaPdb";
+
+declare global {
+    const d3: typeof d3Module;
+}
 
 export const ProtvistaPdbValidation: React.FC<ProtvistaPdbProps> = React.memo(props => {
     console.log("tracks", props.pdb.tracks);
 
+    const svgRef = React.useRef<SVGSVGElement>(null);
+
+    React.useEffect(() => {
+        if (!svgRef.current) return;
+
+        const svg = d3.select(svgRef.current);
+        svg.append("circle").attr("cx", 140).attr("cy", 70).attr("r", 40).style("fill", "red");
+        svg.append("circle").attr("cx", 300).attr("cy", 100).attr("r", 40).style("fill", "green");
+    }, []);
+
     return (
         <>
             <h2>This is the custom ProtvistaPdbValidation component</h2>
+            <svg ref={svgRef} width={5000} height={200} />
             <ProtvistaPdb {...props} />
         </>
     );
