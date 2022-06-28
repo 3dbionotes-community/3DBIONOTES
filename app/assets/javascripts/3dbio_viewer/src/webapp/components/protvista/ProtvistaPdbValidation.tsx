af794e375f9bc1224b3c71f364a4347ee8769126
deeonwuli <onwulidumebi@gmail.com>
2022-06-28 15:05:43 +0100

    Update ProtvistaPdbValidation.tsx

--- app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/ProtvistaPdbValidation.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/ProtvistaPdbValidation.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as d3Module from "d3";
 import { ProtvistaPdb, ProtvistaPdbProps } from "./ProtvistaPdb";
-import modelQualityStats from '../../../data/repositories/emv_modelquality_stats.json'
+import modelQualityStats from "../../../data/repositories/emv_modelquality_stats.json";
 
 declare global {
     const d3: typeof d3Module;
@@ -12,7 +12,7 @@ const margin = {
     right: 10,
     bottom: 10,
     left: 20,
-}
+};
 
 const dimensions = {
     width: 200 - margin.left - margin.right,
@@ -33,7 +33,7 @@ export const ProtvistaPdbValidation: React.FC<ProtvistaPdbProps> = React.memo(pr
 
 function useGrid() {
     const svgRef = React.useRef<SVGSVGElement>(null);
-    
+
     React.useEffect(() => {
         if (!svgRef.current) return;
 
@@ -42,7 +42,7 @@ function useGrid() {
         const numCols = modelQualityStats.graph.axis_x.categories.length;
         const axisX = modelQualityStats.graph.axis_x.axis_name;
         const axisY = modelQualityStats.graph.axis_y.axis_name;
-        
+
         const gridData = () => {
             const data: any[] = [];
             const width = 40;
@@ -60,13 +60,23 @@ function useGrid() {
                         width: width,
                         height: height,
                         value: modelQualityStats.data.categories[i]?.count,
-                        color: (cellXValue === "1" || cellXValue === "3")  && (cellYValue === "1" || cellYValue === "2") ? 'red' : (cellXValue === "1" || cellXValue === "3") && (cellYValue === "3") ? "orange" : (cellXValue === "2" && cellYValue === "4") ? "lime" : (cellXValue === "2" && cellYValue === "3") ? "greenyellow" : 'yellow',
+                        color:
+                            (cellXValue === "1" || cellXValue === "3") &&
+                            (cellYValue === "1" || cellYValue === "2")
+                                ? "red"
+                                : (cellXValue === "1" || cellXValue === "3") && cellYValue === "3"
+                                ? "orange"
+                                : cellXValue === "2" && cellYValue === "4"
+                                ? "lime"
+                                : cellXValue === "2" && cellYValue === "3"
+                                ? "greenyellow"
+                                : "yellow",
                     });
                     xPos += width;
                     i++;
                 }
                 if (row === 1) {
-                    height= 20;
+                    height = 20;
                 } else {
                     height = 40;
                 }
@@ -74,29 +84,28 @@ function useGrid() {
                 yPos -= height;
             }
             return data;
-        }
+        };
 
-        const xScale = d3.scaleLinear()
+        const xScale = d3
+            .scaleLinear()
             .domain([-3.5, 3.5])
             .range([0, numCols * 40]);
-        const xAxis = d3.axisBottom(xScale)
-            .tickValues([-1, 1.5]);
+        const xAxis = d3.axisBottom(xScale).tickValues([-1, 1.5]);
 
-        const yScale = d3.scaleLinear()
+        const yScale = d3
+            .scaleLinear()
             .domain([-0.65, 1.1])
             .range([numRows * 40, 0]);
-        const yAxis = d3.axisLeft(yScale)
-            .tickValues([0.25, 0.45]);
-        
-        const grid = d3.select(svgRef.current)
+        const yAxis = d3.axisLeft(yScale).tickValues([0.25, 0.45]);
+
+        const grid = d3
+            .select(svgRef.current)
             .attr("width", dimensions.width + margin.left + margin.right)
-            .attr("height", dimensions.height +  margin.top + margin.bottom)
+            .attr("height", dimensions.height + margin.top + margin.bottom)
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            
-        grid.append("g")
-            .attr("transform", "translate(30, 10)")
-            .call(yAxis)
-        
+
+        grid.append("g").attr("transform", "translate(30, 10)").call(yAxis);
+
         grid.append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 20)
@@ -112,7 +121,7 @@ function useGrid() {
             .style("text-anchor", "middle")
             .attr("font-size", "0.35em")
             .text("overfitting");
-        
+
         grid.append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 25)
@@ -131,33 +140,37 @@ function useGrid() {
 
         grid.append("g")
             .attr("transform", "translate(30, " + dimensions.width + ")")
-            .call(xAxis)
-        
-        grid.append("text")             
-            .attr("transform", "translate(" + (dimensions.width) + " ," + (dimensions.height + margin.top) + ")")
+            .call(xAxis);
+
+        grid.append("text")
+            .attr(
+                "transform",
+                "translate(" + dimensions.width + " ," + (dimensions.height + margin.top) + ")"
+            )
             .style("text-anchor", "middle")
             .attr("font-size", "0.75em")
             .text(axisX);
 
-        grid.append("text")             
-            .attr("transform", "translate(50, " + (dimensions.height) + ")")
+        grid.append("text")
+            .attr("transform", "translate(50, " + dimensions.height + ")")
             .style("text-anchor", "middle")
             .attr("font-size", "0.35em")
             .text("overfitting");
 
-        grid.append("text")             
-            .attr("transform", "translate(90, " + (dimensions.height) + ")")
+        grid.append("text")
+            .attr("transform", "translate(90, " + dimensions.height + ")")
             .style("text-anchor", "middle")
             .attr("font-size", "0.35em")
             .text("right");
 
-        grid.append("text")             
-            .attr("transform", "translate(135, " + (dimensions.height) + ")")
+        grid.append("text")
+            .attr("transform", "translate(135, " + dimensions.height + ")")
             .style("text-anchor", "middle")
             .attr("font-size", "0.35em")
             .text("wrong");
-       
-        const row = grid.append("g")
+
+        const row = grid
+            .append("g")
             .attr("transform", "translate(30, -" + margin.bottom * 3 + ")")
             .selectAll(".row")
             .data(gridData)
@@ -165,20 +178,21 @@ function useGrid() {
             .append("g")
             .attr("class", "row");
 
-        const cell = row.selectAll(".cell")
-            .data((d) => d)
+        const cell = row
+            .selectAll(".cell")
+            .data(d => d)
             .enter()
             .append("g")
-            .attr("class", ".cell")
+            .attr("class", ".cell");
 
         cell.append("rect")
             .attr("x", (d: any) => d.x)
             .attr("y", (d: any) => d.y)
-            .attr("width", (d: any) =>  d.width)
+            .attr("width", (d: any) => d.width)
             .attr("height", (d: any) => d.height)
             .attr("fill", (d: any) => d.color)
             .style("stroke", "#222");
-        
+
         cell.append("text")
             .attr("x", (d: any) => d.x + 8)
             .attr("y", (d: any) => d.y + 10)
