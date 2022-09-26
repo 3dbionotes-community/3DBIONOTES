cccc57073b9367ee6a71312f42fd32f8ce0c551c
p3rcypj <pablo@percypj.com>
2022-09-26 13:13:48 +0000

    Fix warning

--- app/assets/javascripts/covid19/src/routes.ts
+++ app/assets/javascripts/covid19/src/routes.ts
@@ -1,7 +1,7 @@
 /* On DEV, proxy requests (to circumvent CORS) and cache them (see src/setupProxy.js) */
 
 const isDev = process.env.NODE_ENV === "development";
-const isOldViewer = true;
+const _isOldViewer = true;
 
 export const routes = {
     bionotesApi: isDev ? "http://rinchen-dos.cnb.csic.es/bws/api/" : "",
