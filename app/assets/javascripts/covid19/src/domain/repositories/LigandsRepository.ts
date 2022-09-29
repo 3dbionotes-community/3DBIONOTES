20a96c1c47f700270991c567a78f89611f9931b8
p3rcypj <pablo@percypj.com>
2022-09-29 13:06:51 +0000

    Update domain repo and use case

--- app/assets/javascripts/covid19/src/domain/repositories/LigandsRepository.ts
+++ app/assets/javascripts/covid19/src/domain/repositories/LigandsRepository.ts
@@ -1,3 +1,7 @@
+import { FutureData } from "../../data/utils/future";
+import { LigandImageData } from "../entities/LigandImageData";
+import { Maybe } from "../../data/utils/ts-utils";
+
 export interface LigandsRepository {
-    getImageDataResource: () => void;
+    getImageDataResource: (id: string) => FutureData<Maybe<LigandImageData>>;
 }
--- app/assets/javascripts/covid19/src/domain/usecases/GetLigandImageDataResourceUseCase.ts
+++ app/assets/javascripts/covid19/src/domain/usecases/GetLigandImageDataResourceUseCase.ts
@@ -3,7 +3,7 @@ import { LigandsRepository } from "../repositories/LigandsRepository";
 export class GetLigandImageDataResourceUseCase {
     constructor(private ligandsRepository: LigandsRepository) {}
 
-    execute() {
-        return this.ligandsRepository.getImageDataResource();
+    execute(id: string) {
+        return this.ligandsRepository.getImageDataResource(id);
     }
 }
