49211116f9859fcc9678393913f1ac87666ccb1a
p3rcypj <pablo@percypj.com>
2022-09-29 13:08:55 +0000

    Retrieve IDR on row load (NOT WORKING ON FIRST SIGHT)

--- app/assets/javascripts/covid19/src/webapp/components/structures-table/cells/LigandsCell.tsx
+++ app/assets/javascripts/covid19/src/webapp/components/structures-table/cells/LigandsCell.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import { Maybe } from "../../../../data/utils/ts-utils";
+import { LigandImageData } from "../../../../domain/entities/LigandImageData";
 import i18n from "../../../../utils/i18n";
+import { useAppContext } from "../../../contexts/app-context";
 import { BadgeLigands } from "../badge/BadgeLigands";
 import { CellProps, styles } from "../Columns";
 import { Link } from "../Link";
 import { Wrapper } from "./Wrapper";
 
 export const LigandsCell: React.FC<CellProps> = React.memo(props => {
-    const { row, onClickDetails, onClickLigands, moreDetails } = props;
+    const { row, onClickDetails, onClickIDR, moreDetails } = props;
+    const { compositionRoot } = useAppContext();
+    const [ligandsIDR, setLigandsIDR] = React.useState<Record<string, Maybe<LigandImageData>>>({});
 
     const ligands = React.useMemo(() => {
         return row.ligands.map(ligand => {
@@ -38,6 +43,21 @@ export const LigandsCell: React.FC<CellProps> = React.memo(props => {
         });
     }, [row.ligands]);
 
+    React.useEffect(() => {
+        row.ligands.forEach(ligand =>
+            compositionRoot.ligands.getIDR.execute(ligand.id).run(
+                imageDataResource => {
+                    if (imageDataResource)
+                        setLigandsIDR(ligandsIDR => ({
+                            ...ligandsIDR,
+                            [ligand.id]: imageDataResource,
+                        }));
+                },
+                err => console.error(err.message)
+            )
+        );
+    }, [row.ligands, compositionRoot]);
+
     return (
         <Wrapper
             onClickDetails={onClickDetails}
@@ -45,21 +65,27 @@ export const LigandsCell: React.FC<CellProps> = React.memo(props => {
             row={row}
             field="ligands"
         >
-            {ligands.map(ligand => (
-                <LigandItem key={ligand.id} moreDetails={moreDetails}>
-                    <Link
-                        tooltip={ligand.tooltip}
-                        url={ligand.url}
-                        text={`${ligand.name} (${ligand.id})`}
-                    >
-                        <BadgeLigands
-                            ligand={ligand}
-                            onClick={onClickLigands}
-                            moreDetails={moreDetails}
-                        />
-                    </Link>
-                </LigandItem>
-            ))}
+            {ligands.map(ligand => {
+                const idr = ligandsIDR[ligand.id];
+                return (
+                    <LigandItem key={ligand.id} moreDetails={moreDetails}>
+                        <Link
+                            tooltip={ligand.tooltip}
+                            url={ligand.url}
+                            text={`${ligand.name} (${ligand.id})`}
+                        >
+                            {idr && (
+                                <BadgeLigands
+                                    ligand={ligand}
+                                    onClick={onClickIDR}
+                                    moreDetails={moreDetails}
+                                    imageDataResource={idr}
+                                />
+                            )}
+                        </Link>
+                    </LigandItem>
+                );
+            })}
         </Wrapper>
     );
 });
