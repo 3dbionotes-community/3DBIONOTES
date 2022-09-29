64e813c90e99b4659356c626d786989d2f64bb9d
p3rcypj <pablo@percypj.com>
2022-09-29 13:09:35 +0000

    Changes to IDR callback and external link

--- app/assets/javascripts/covid19/src/webapp/components/structures-table/badge/BadgeLigands.tsx
+++ app/assets/javascripts/covid19/src/webapp/components/structures-table/badge/BadgeLigands.tsx
@@ -1,45 +1,43 @@
 import React from "react";
 import styled from "styled-components";
-import { Badge } from "./Badge";
-import { Ligand } from "../../../../domain/entities/Covid19Info";
 import i18n from "../../../../utils/i18n";
+import { Ligand } from "../../../../domain/entities/Covid19Info";
+import { LigandImageData } from "../../../../domain/entities/LigandImageData";
+import { BadgeLink } from "./BadgeLink";
+import { IDROptions } from "../Columns";
+import { Badge } from "./Badge";
 
 export interface BadgeLigandsProps {
-    onClick?: (options: { ligand: Ligand }) => void;
+    onClick?: (options: IDROptions) => void;
     ligand: Ligand;
     moreDetails?: boolean;
+    imageDataResource: LigandImageData;
 }
 
 export const BadgeLigands: React.FC<BadgeLigandsProps> = React.memo(props => {
-    const { ligand, onClick, moreDetails = true } = props;
-    console.log(moreDetails);
+    const { ligand, onClick, moreDetails = true, imageDataResource: idr } = props;
+
     const notifyClick = React.useCallback(
         e => {
             e.preventDefault();
-            onClick?.({ ligand });
+            onClick?.({ ligand, idr });
         },
-        [onClick, ligand]
+        [onClick, ligand, idr]
     );
-
-    const openExternal = React.useCallback(() => {}, []);
 
     return moreDetails ? (
         <BadgeGroup>
             <Badge onClick={notifyClick} backgroundColor={"w3-cyan"}>
                 {i18n.t("IDR")} <i className="fa fa-info-circle icon-right"></i>
             </Badge>
-            <Badge onClick={openExternal} backgroundColor={"w3-cyan"}>
-                <i className="fa fa-external-link-square"></i>
-            </Badge>
+            <BadgeLink url={idr.externalLink} backgroundColor={"w3-cyan"} icon="external" />
         </BadgeGroup>
     ) : (
         <BadgeInlineGroup>
             <Badge onClick={notifyClick} backgroundColor={"w3-cyan"}>
                 {i18n.t("IDR")} <i className="fa fa-info-circle icon-right"></i>
             </Badge>
-            <Badge onClick={openExternal} backgroundColor={"w3-cyan"}>
-                <i className="fa fa-external-link-square"></i>
-            </Badge>
+            <BadgeLink url={idr.externalLink} backgroundColor={"w3-cyan"} icon="external" />
         </BadgeInlineGroup>
     );
 });
@@ -51,5 +49,6 @@ const BadgeGroup = styled.div`
 
 const BadgeInlineGroup = styled.div`
     display: inline-flex;
+    align-items: flex-end;
     justify-content: center;
 `;
