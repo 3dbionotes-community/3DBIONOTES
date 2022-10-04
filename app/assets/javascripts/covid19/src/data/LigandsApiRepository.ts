8cc666934b34e8020da7d3fe153af8d7c0e1783e
p3rcypj <pablo@percypj.com>
2022-10-04 13:52:51 +0000

    Compound mapping and check

--- app/assets/javascripts/covid19/src/data/LigandsApiRepository.ts
+++ app/assets/javascripts/covid19/src/data/LigandsApiRepository.ts
@@ -32,18 +32,56 @@ export class LigandsApiRepository implements LigandsRepository {
                 const idr: any = _.first(imageData);
                 return {
                     ...idr,
-                    assays: idr.assays.map((assay: any) => ({
-                        ...assay,
-                        id: assay.dbId,
-                        type: assay.assayType,
-                        typeTermAccession: assay.assayTypeTermAccession,
-                        screens: assay.screens.map((screen: any) => ({
-                            ...screen,
-                            id: screen.dbId,
-                            doi: screen.dataDoi,
-                        })),
-                        compound: {},
-                    })),
+                    assays: idr.assays.map((assay: any) => {
+                        const wellsOnPlates: any[][] = assay.screens
+                            .find((screen: any) => screen.dbId === "2602")
+                            ?.plates.map((plate: any) => plate.wells);
+                        console.log(wellsOnPlates);
+                        const allPercentageInhibition = wellsOnPlates
+                            ?.map((plate: any) =>
+                                plate.flatMap((well: any) =>
+                                    well.percentageInhibition
+                                        ? `${well.percentageInhibition}%`
+                                        : undefined
+                                )
+                            )
+                            .join(", ");
+                        console.log(allPercentageInhibition);
+
+                        const cytotoxicity = assay.additionalAnalyses.find(
+                            (analytic: any) => analytic.name === "CC50"
+                        );
+                        const doseResponse = assay.additionalAnalyses.find(
+                            (analytic: any) => analytic.name === "IC50"
+                        );
+                        const cytotoxicIndex = assay.additionalAnalyses.find(
+                            (analytic: any) => analytic.name === "Selectivity index"
+                        );
+
+                        return {
+                            ...assay,
+                            id: assay.dbId,
+                            type: assay.assayType,
+                            typeTermAccession: assay.assayTypeTermAccession,
+                            screens: assay.screens.map((screen: any) => ({
+                                ...screen,
+                                id: screen.dbId,
+                                doi: screen.dataDoi,
+                            })),
+                            compound: {
+                                percentageInhibition: allPercentageInhibition,
+                                cytotoxicity: cytotoxicity
+                                    ? `${cytotoxicity.value} ${cytotoxicity.units ?? ""}`
+                                    : undefined,
+                                cytotoxicityIndex: cytotoxicIndex
+                                    ? `${cytotoxicIndex.value} ${cytotoxicIndex.units ?? ""}`
+                                    : undefined,
+                                doseResponse: doseResponse
+                                    ? `${doseResponse.value} ${doseResponse.units ?? ""}`
+                                    : undefined,
+                            },
+                        };
+                    }),
                 } as LigandImageData;
             });
         else return Future.success(undefined);
