582ee99e18dd543d5daa0945ebd726012f8a00e2
p3rcypj <pablo@percypj.com>
2022-11-03 12:06:56 +0000

    Update prod api url

--- app/assets/javascripts/covid19/src/routes.ts
+++ app/assets/javascripts/covid19/src/routes.ts
@@ -3,5 +3,7 @@
 const isDev = process.env.NODE_ENV === "development";
 
 export const routes = {
-    bionotesApi: isDev ? "http://rinchen-dos.cnb.csic.es/bws/api" : "",
+    bionotesApi: isDev
+        ? "http://rinchen-dos.cnb.csic.es/bws/api"
+        : "http://rinchen-dos.cnb.csic.es/bws/api", //change it on future when JR turns api to prod
 };
