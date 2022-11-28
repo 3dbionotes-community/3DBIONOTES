101dd78a4206259de7c99c30f54b4074aedc43fd
p3rcypj <pablo@percypj.com>
2022-11-28 02:30:10 +0000

    Update routes

--- app/assets/javascripts/3dbio_viewer/src/routes.ts
+++ app/assets/javascripts/3dbio_viewer/src/routes.ts
@@ -4,7 +4,9 @@ const isDev = process.env.NODE_ENV === "development";
 
 // If empty, use relative requests.
 export const routes = {
-    publicBionotesDev: "http://rinchen-dos.cnb.csic.es/bws", //change for prod url
+    publicBionotesDev: isDev
+        ? "http://rinchen-dos.cnb.csic.es/bws/"
+        : "http://3dbionotes.cnb.csic.es/bws", //change for prod url
     bionotes: isDev ? "/3dbionotes" : "",
     bionotesDev: isDev ? "/rinchen-dos" : "",
     ebi: isDev ? "/ebi" : "https://www.ebi.ac.uk",
