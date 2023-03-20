3c85bd87f228432f64cf84cba14d4e1ad0ec1a8f
p3rcypj <pablo@percypj.com>
2023-03-20 16:35:04 +0000

    gtag on dev

--- app/assets/javascripts/covid19/src/utils/analytics.ts
+++ app/assets/javascripts/covid19/src/utils/analytics.ts
@@ -1,14 +1,21 @@
+declare global {
+    interface Window {
+        gtag?: (...args: []) => void;
+    }
+}
+
 declare function gtag(type: "event", name: string, data?: object): void;
 
 export function sendAnalytics(name: string, data?: object) {
-    gtag(
-        "event",
-        name,
-        data && {
-            ...data,
-            location_hash: window.location.hash,
-            location_pathname: window.location.pathname,
-            location_href: window.location.href,
-        }
-    );
+    if (window.gtag !== undefined)
+        gtag(
+            "event",
+            name,
+            data && {
+                ...data,
+                location_hash: window.location.hash,
+                location_pathname: window.location.pathname,
+                location_href: window.location.href,
+            }
+        );
 }
