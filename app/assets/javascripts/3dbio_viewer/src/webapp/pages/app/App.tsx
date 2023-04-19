cb8d847a716daf9243324c86874ef0da77f9e1d9
p3rcypj <pablo@percypj.com>
2023-04-19 18:31:20 +0000

    App router

--- app/assets/javascripts/3dbio_viewer/src/webapp/pages/app/App.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/pages/app/App.tsx
@@ -1,42 +1,17 @@
 import React from "react";
-import { HashRouter, Redirect, Route, Switch, useLocation } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AppContext } from "../../../webapp/components/AppContext";
-import { ProtvistaGrouped } from "../../components/protvista/ProvistaGrouped";
-import { RootViewer } from "../../components/RootViewer";
-import { sendAnalytics } from "../../utils/analytics";
+import AppRouter from "./AppRouter";
 import "./App.css";
 
 function App() {
     return (
         <AppContext>
             <HashRouter>
-                <Switch>
-                    <RouterTracking>
-                        <Route
-                            path="/protvista-all/:selection"
-                            render={() => <ProtvistaGrouped />}
-                        />
-                        <Route
-                            path="/uploaded/:token"
-                            render={() => <RootViewer from="uploaded" />}
-                        />
-                        <Route
-                            path="/network/:token"
-                            render={() => <RootViewer from="network" />}
-                        />
-                        <Route path="/:selection" render={() => <RootViewer from="selector" />} />
-                        <Route exact path="/">
-                            <Redirect to="/6zow+EMD-11328" />
-                        </Route>
-                    </RouterTracking>
-                </Switch>
+                <AppRouter />
             </HashRouter>
         </AppContext>
     );
 }
 
-const RouterTracking: React.FC = props => {
-    return <>{props.children}</>;
-};
-
-export default App;
+export default React.memo(App);
