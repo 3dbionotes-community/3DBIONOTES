8cadffdace2744a8db3a2cd0daa062712dae76b8
p3rcypj <pablo@percypj.com>
2023-07-09 15:04:15 +0000

    Remove network tool

--- app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/ToolsButton.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/ToolsButton.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import i18n from "../../utils/i18n";
 import { Dropdown, DropdownProps } from "../dropdown/Dropdown";
-import { Network } from "../network/Network";
 import { useBooleanState } from "../../hooks/use-boolean";
-import { sendAnalytics } from "../../utils/analytics";
 import { AnnotationsTool } from "../annotations-tool/AnnotationsTool";
 import { Annotations } from "../../../domain/entities/Annotation";
 import { Build as BuildIcon } from "@material-ui/icons";
@@ -19,33 +17,20 @@ type Props = DropdownProps<ItemId>;
 export const ToolsButton: React.FC<ToolsButtonProps> = props => {
     const { onAddAnnotations } = props;
 
-    const [isNetworkOpen, { open: openNetwork, close: closeNetwork }] = useBooleanState(false);
     const [isAnnotationToolOpen, annotationToolActions] = useBooleanState(false);
 
     const items = React.useMemo<Props["items"]>(() => {
-        return [
-            { text: i18n.t("Upload custom annotations"), id: "custom-annotations" },
-            { text: i18n.t("Network"), id: "network" },
-        ];
+        return [{ text: i18n.t("Upload custom annotations"), id: "custom-annotations" }];
     }, []);
-
-    const openNetworkWithAnalytics = React.useCallback(() => {
-        openNetwork();
-        sendAnalytics("open_dialog", {
-            label: "Network",
-        });
-    }, [openNetwork]);
 
     const openMenuItem = React.useCallback<Props["onClick"]>(
         itemId => {
             switch (itemId) {
                 case "custom-annotations":
                     return annotationToolActions.open();
-                case "network":
-                    return openNetworkWithAnalytics();
             }
         },
-        [annotationToolActions, openNetworkWithAnalytics]
+        [annotationToolActions]
     );
 
     return (
@@ -56,8 +41,6 @@ export const ToolsButton: React.FC<ToolsButtonProps> = props => {
                 onClick={openMenuItem}
                 leftIcon={<BuildIcon fontSize="small" />}
             />
-
-            {isNetworkOpen && <Network onClose={closeNetwork} />}
 
             {isAnnotationToolOpen && (
                 <AnnotationsTool onClose={annotationToolActions.close} onAdd={onAddAnnotations} />
