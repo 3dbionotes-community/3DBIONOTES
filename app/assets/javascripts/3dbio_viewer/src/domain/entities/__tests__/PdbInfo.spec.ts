c5f7c65867e58ed73858c4cbc6859b03e720f67a
p3rcypj <pablo@percypj.com>
2024-01-08 09:46:50 +0000

    Add first test

--- app/assets/javascripts/3dbio_viewer/src/domain/entities/__tests__/PdbInfo.spec.ts
+++ app/assets/javascripts/3dbio_viewer/src/domain/entities/__tests__/PdbInfo.spec.ts
@@ -1,11 +1,16 @@
 import { describe, expect, it } from "@jest/globals";
 import { getCompositionRoot } from "../../../compositionRoot";
+import { defaultPdbId } from "../../../webapp/pages/app/AppRouter";
 
 describe("PdbInfo", () => {
-    it("with PDB 6zow is truthy", async () => {
+    it("with PDB 6zow to be truthy", async () => {
         const compositionRoot = getCompositionRoot();
-        const pdbInfo = await compositionRoot.getPdbInfo.execute("6zow").toPromise();
+        const pdbInfo = await compositionRoot.getPdbInfo.execute(defaultPdbId).toPromise();
 
         expect(pdbInfo).toBeTruthy();
     });
+    // it("with PDB 9zzz rejects", async () => {
+    //     const compositionRoot = getCompositionRoot();
+    //     expect(compositionRoot.getPdbInfo.execute("9zzz").toPromise()).rejects;
+    // });
 });
