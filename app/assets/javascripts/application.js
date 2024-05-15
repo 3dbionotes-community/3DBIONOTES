53dd8a1cc2694725559cdd65a836563e098292ad
p3rcypj <pablo@percypj.com>
2024-05-15 10:40:24 +0000

    Not working webpack

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,6 @@
 // Changed from 'sprockets' to 'webpack'
+import * as scss from "../stylesheets/application.scss";
+
 import $ from 'jquery'
 import {} from 'jquery-ujs'
 import 'bootstrap'
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,31 @@ module.exports = {
           "sass-loader",
         ],
       },
+      // {
+      //   test: /\.((c|sa|sc)ss)$/i,
+      //   use: [
+      //     "style-loader",
+      //     {
+      //       loader: "css-loader",
+      //       options: {
+      //         // Run `postcss-loader` on each CSS `@import` and CSS modules/ICSS imports, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
+      //         // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
+      //         importLoaders: 1,
+      //       },
+      //     },
+      //     {
+      //       loader: "postcss-loader",
+      //       options: { plugins: () => [postcssPresetEnv({ stage: 0 })] },
+      //     },
+      //     {
+      //       loader: "sass-loader",
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: "asset",
+      },
     ],
   },
   plugins: [].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
