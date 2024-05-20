309d9d97e651ab4482144c41b2565f6daa3fc4d3
p3rcypj <pablo@percypj.com>
2024-05-20 21:54:20 +0000

    Disclaimer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
+// Bootstrap 4 depends on jquery3, but uses of jquery
+// outside of bootstrap are not checked and we do not
+// use as much functionality in bootstrap. Please note
+// that if some bootstrap functionality is not working,
+// it could potentially be caused by this.
+
 //= require jquery
 //= require jquery_ujs
 //= require popper
