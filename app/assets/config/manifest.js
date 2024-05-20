16ea9e64eeaa13c7ba8fc716a2af7ce83c2321c4
p3rcypj <pablo@percypj.com>
2024-05-20 22:36:01 +0000

    Move precompile assets to manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,15 +1,97 @@
 //= link_tree ../images
 //= link_tree ../stylesheets
+
+//= link global_tools/sweetalert.min.js
+//= link global_tools/sweetalert.css
+
+//= link main_frame/main_lib.js
+//= link main_frame/ppi_annotations.js
+//= link main_frame/import_proteins.js
+//= link main_frame/upload_data.js
+//= link main_frame/remove_annotations.js
+//= link main_frame/main_init.js
+//= link main_frame/main_inter.js
+//= link main_frame/main_style.css
+//= link main_frame/upload_data.css
+//= link main_frame/remove_annotations.css
+//= link main_frame/import_proteins.css
+//= link main_frame/fontello.css
+//= link main_frame/upload_first_step_form.css
+
+//= link sequence_viewer/jquery-1.4.2.min.js
+//= link sequence_viewer/jquery-ui-1.8.2.custom.min.js
+//= link sequence_viewer/Biojs.js
+//= link sequence_viewer/Biojs.Sequence_alignment.js
+//= link sequence_viewer/Biojs.Tooltip.js
+//= link sequence_viewer/biojs_sequence.css
+//= link sequence_frame/sequence_lib.js
+//= link sequence_frame/sequence_init.js
+//= link sequence_frame/sequence_inter.js
+
+//= link jsmol/jquery/jquery.js
+//= link jsmol/js/JSmoljQueryExt.js
+//= link jsmol/JSmol.min.js
+//= link jsmol/JSmol.GLmol.min.js
+
+//= link jsmol_viewer/chimeraClass.js
+//= link jsmol_viewer/JSmolClass.js
+//= link jsmol_viewer/bionotes_jsmol.js
+
+//= link jsmol_frame/jsmol_style.css
+//= link jsmol_frame/jsmol_lib.js
+//= link jsmol_frame/jsmol_init.js
+//= link jsmol_frame/jsmol_inter.js
+
+//= link annotations_frame/annotations_lib.js
+//= link annotations_frame/annotations_init.js
+//= link annotations_frame/annotations_inter.js
+//= link annotations_frame/annotations_style.css
+//= link annotations_viewer/protvista.js
+//= link annotations_viewer/featuresAnalysis.bundle.js
+//= link annotations_viewer/analyse_data.js
+//= link annotations_viewer/extendProtVista.bundle.js
+//= link annotations_viewer/extend_data.js
+//= link annotations_viewer/extend_style.css
+//= link annotations_viewer/uniprot_viewer_main.css
+//= link annotations_viewer/features_analysis.css
+
+//= link genomic_viewer/genefeaturesviewer.js
+//= link genomic_viewer/genomic_viewer_main.css
+//= link genomic_viewer/genomic_viewer_extension.css
+//= link genomic_frame/genomic_style.css
+//= link genomic_frame/genomic_lib.js
+//= link genomic_frame/genomic_init.js
+//= link genomic_frame/genomic_inter.js
+
+//= link ppi_frame/ppi_style.css
+//= link ppi_frame/cytoscape.min.js
+//= link ppi_frame/rhill-voronoi-core.js
+//= link ppi_frame/foograph.js
+//= link ppi_frame/ppi_lib.js
+//= link ppi_frame/ppi_init.js
+//= link ppi_frame/ppi_inter.js
+//= link ppi_frame/graph_class.js
+//= link interface_viewer/interface_viewer.bundle.js
+
 //= link webserver/webserver_style.css
+//= link webserver/w3.css
+//= link webserver/help_style.css
+//= link webserver/home_style.css
 //= link webserver/webserver_lib.js
 //= link webserver/webserver_init.js
+
+//= link webserver/main.css
+//= link webserver/ekko-lightbox.css
+//= link webserver/proteome.css
+//= link webserver/carousel.css
+//= link webserver/imagemapResizer.js
+//= link webserver/carousel.js
+//= link webserver/popover.js
+//= link webserver/collapse.js
 //= link webserver/ekko-lightbox.min.js
 //= link webserver/lightbox.js
-//= link webserver/carousel.js
-//= link webserver/collapse.js
-//= link webserver/imagemapResizer.js
 //= link covid19.js
-//= link webserver/main.css
-//= link covid19/build/app/2.chunk
-//= link covid19/build/app/main.chunk.js
-//= link covid19/build/app/runtime-main.js
+
+//= link webserver/viewer.css
+//= link_tree ../3dbio_viewer/build
+//= link_tree ../covid19/build
