0f75f8275966ed154eab0020d0be035061917a5a
p3rcypj <pablo@percypj.com>
2024-06-20 14:53:47 +0000

    Harcoded reference

--- app/lib/annotation_manager/source_protein_data/source_iedb.rb
+++ app/lib/annotation_manager/source_protein_data/source_iedb.rb
@@ -3,9 +3,10 @@ module AnnotationManager
     module SourceIedb 
       def sourceIedbFromUniprot(uniprotAc)
         client = Mysql2::Client.new(
-          username: "bionotes",
-          database: "IEDB",
-          password: "bionotes-1"
+          host:     ENV['DB_HOST'],
+          username: ENV['DB_USER'],
+          database: ENV['DB_NAME'],
+          password: ENV['DB_PASSWD']
         )
         out = []
         query = "select distinct e.epitope_id, ee.linear_peptide_seq, o.starting_position, o.ending_position from epitope ee, epitope_object e, object o where ee.epitope_id=e.epitope_id and  e.object_id=o.object_id and e.source_antigen_accession in (\""+uniprotAc+"\",\""+uniprotAc+".1\") and o.object_type = \"Fragment of a Natural Sequence Molecule\";"
