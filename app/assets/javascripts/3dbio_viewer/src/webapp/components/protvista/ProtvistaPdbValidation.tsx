a8ce9dd6c3cacd037d6cfdca86efbbae6d46552b
p3rcypj <pablo@percypj.com>
2024-12-31 11:24:20 +0000

    Comment some hidden grid

--- app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/ProtvistaPdbValidation.tsx
+++ app/assets/javascripts/3dbio_viewer/src/webapp/components/protvista/ProtvistaPdbValidation.tsx
@@ -13,7 +13,7 @@ declare global {
 
 export const ProtvistaPdbValidation: React.FC<ProtvistaPdbProps> = React.memo(props => {
     const stats = _.first(props.pdb.emdbs)?.emv?.stats;
-    const _ref = useGrid(); /*temporal hidden*/
+    /* const _ref = useGrid(); /* temporal hidden */
 
     return (
         <>
