2ecf5e89612eaeac3be8d632473b92b7c1957431
p3rcypj <pablo@percypj.com>
2025-01-27 16:18:29 +0000

    Fix tests

--- app/assets/javascripts/3dbio_viewer/src/domain/entities/__tests__/PdbInfo.spec.ts
+++ app/assets/javascripts/3dbio_viewer/src/domain/entities/__tests__/PdbInfo.spec.ts
@@ -7,7 +7,12 @@ describe("PdbInfo", () => {
     it("with PDB 6zow to be truthy", async () => {
         axios.defaults.adapter = require("axios/lib/adapters/http");
         const compositionRoot = getCompositionRoot();
-        const pdbInfo = await compositionRoot.getPdbInfo.execute(defaultPdbId).toPromise();
+        const pdbInfo = await compositionRoot.getPdbInfo
+            .execute({
+                pdbId: defaultPdbId,
+                onProcessDelay: (_reason: string) => {},
+            })
+            .toPromise();
 
         return expect(pdbInfo).toBeTruthy();
     });
